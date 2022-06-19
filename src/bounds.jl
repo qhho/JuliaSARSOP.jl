cbb02036024f7008df6fbe8fca5935943b79e0e6
Zakariya Laouar <zala9003@colorado.edu>
2022-06-19 16:32:46 -0600

    sawtooth first pass

--- src/bounds.jl
+++ src/bounds.jl
@@ -1,5 +1,5 @@
 function initUpperBound!(tree::SARSOPTree, b_idx::Int)
-    b = b[b_idx]
+    b = tree.b[b_idx]
     tmp = 0.0
     for i in 1:length(values) 
         tmp += values[i] * b[i]
@@ -9,6 +9,20 @@ end
 
 function getUpperBoundSimple(values::Vector{Float64}, b::Vector{Float64})
         return sum(values .* b)
+end
+
+function sawtoothUB!(tree::SARSOPTree, b_idx::Int)
+    b = tree.b[b_idx]
+    α_corner = tree.Vs_upper
+    V_corner = b .* α_corner
+    V_upper = tree.V_upper
+
+    upperVvec = Float64[]
+    for (bint, vint) in zip(tree.b, V_upper)  
+        ϕ = minimum(b[s]/bint[s] for s in 1:length(b))
+        push!(upperVvec, V_corner + ϕ * (vint - (bint .* α_corner)))
+    end
+    tree.V_upper[b_idx] = minimum(upperVvec)
 end
 
 # Get upper bound value for each belief in tree
