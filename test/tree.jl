9acf78026a3b25c01e83965d8dc9c93964f514c6
WhiffleFish <tylertronics@gmail.com>
2022-10-24 17:02:34 -0600

    fix tests + remove support for julia 1.6

--- test/tree.jl
+++ test/tree.jl
@@ -1,4 +1,9 @@
 @testset "tree" begin
+    function jsop_obs_prob(tree::SARSOPTree, b, a::Int, o::Int)
+        pred = JSOP.predictor(tree.pomdp, sparse(b), a)
+        bp = JSOP.corrector(tree.pomdp, pred, a, o)
+        return sum(bp)
+    end
     @testset "poba" begin
         pomdp = TigerPOMDP()
         bu = DiscreteUpdater(pomdp)
@@ -7,9 +12,11 @@
         b0 = initialstate(pomdp)
         b0_vec = initialize_belief(DiscreteUpdater(pomdp), b0).b
         for a ∈ actions(pomdp)
+            a_idx = actionindex(pomdp, a)
             prob_sum = 0.0
             for o ∈ observations(pomdp)
-                poba = JSOP.obs_prob(tree, b0_vec, a, o)
+                o_idx = obsindex(pomdp, o)
+                poba = jsop_obs_prob(tree, b0_vec, a_idx, o_idx)
                 @test 0. ≤ poba ≤ 1.
                 prob_sum += poba
             end
@@ -22,18 +29,6 @@
         pomdp = TigerPOMDP()
         sol = SARSOPSolver()
         tree = SARSOPTree(pomdp)
-
-        S = states(tree)
-        A = actions(tree)
-        O = observations(tree)
-        # make sure ordering is working as intended
-        # So we don't have to constantly call possibly allocating functions: `ordered_foo`, `fooindex`
-        @test all(S .== ordered_states(pomdp))
-        @test all([stateindex(pomdp, s) for s ∈ S] .== eachindex(S))
-        @test all(A .== ordered_actions(pomdp))
-        @test all([actionindex(pomdp, a) for a ∈ A] .== eachindex(A))
-        @test all(O .== ordered_observations(pomdp))
-        @test all([obsindex(pomdp, o) for o ∈ O] .== eachindex(O))
 
         # consistent sizing
         JSOP.sample!(sol, tree)
@@ -55,12 +50,12 @@
         γ = discount(tree)
         Lp,Up = Rba,Rba
 
-        for (o_idx,o) ∈ enumerate(observations(tree))
+        for o_idx ∈ observations(tree)
             if o_idx == op_idx
                 Lp += γ*JSOP.obs_prob(tree, ba_idx, op_idx)*Lt
                 Up += γ*JSOP.obs_prob(tree, ba_idx, op_idx)*Ut
             else
-                bp_idx = tree.ba_children[ba_idx][o_idx].second
+                bp_idx = tree.ba_children[ba_idx][o_idx]
                 Lp += γ*JSOP.obs_prob(tree, ba_idx, op_idx)*tree.V_lower[bp_idx]
                 Up += γ*JSOP.obs_prob(tree, ba_idx, op_idx)*tree.V_upper[bp_idx]
             end
@@ -84,7 +79,8 @@
 
         JSOP.fill_belief!(tree, b_idx)
         Q̲, Q̄, ap_idx = JSOP.max_r_and_q(tree, b_idx)
-        a′, ba_idx = tree.b_children[b_idx][ap_idx]
+        ba_idx = tree.b_children[b_idx][ap_idx]
+        a′ = tree.ba_action[ba_idx]
         Rba′ = JSOP.belief_reward(tree, tree.b[b_idx], a′)
         L′ = max(L, Q̲)
         U′ = max(U, Q̲ + γ^(-t)*ϵ)
